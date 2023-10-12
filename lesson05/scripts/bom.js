6f2b69728712a64fde123de197451b7f82653beb
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-10-12 11:40:27 +0200

    corrected js file

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -3,22 +3,28 @@ const button = document.querySelector('#button')
 const list = document.querySelector('.list')
 
 button.addEventListener("click", () => {
-    myBook = input.value;
+
+    const myBook = input.value;
+    if (myBook.trim() == "") {
+        alert("Please add a book and chapter before adding.");
+        return
+    }
+
     input.value = "";
 
-    let book = document.createElement('li')
-    let text = document.createElement('span')
-    let button = document.createElement('button')
+    let book = document.createElement('li');
+    let text = document.createElement('span');
+    let removeButton = document.createElement('button');
 
     book.appendChild(text);
     text.textContent = myBook;
-    book.appendChild(button);
-    button.textContent = "X";
-    list.appendChild(book)
+    book.appendChild(removeButton);
+    removeButton.textContent = "X";
+    list.appendChild(book);
 
-    button.addEventListener('click', () => {
-        list.removeChild(book)
+    removeButton.addEventListener('click', () => {
+        list.removeChild(book);
     });
-    
+
     input.focus();
 })
