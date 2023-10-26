f3fafd10b3af1854782b4f3efbef446c2cf8a412
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-10-26 20:08:56 +0200

    added placeholder discover page, edited wdd home

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,9 +1,37 @@
 function toggleDarkMode() {
     var body = document.body;
-    body.classList.toggle("dark-mode");
+    var isDarkMode = body.classList.toggle("dark-mode");
 
-    var darkElements = document.querySelectorAll("dark-element");
+    localStorage.setItem("darkMode", isDarkMode);
+    
+    var darkElements = document.querySelectorAll(".dark-element");
     darkElements.forEach(function(element) {
         element.classList.toggle("dark-mode");
-    })
+    });
 }
+
+var isDarkModePreferred = localStorage.getItem("darkMode") === "true";
+
+var body = document.body;
+body.classList.toggle("dark-mode", isDarkModePreferred);
+
+var darkElements = document.querySelectorAll(".dark-element");
+darkElements.forEach(function(element) {
+    element.classList.toggle("dark-mode", isDarkModePreferred);
+});
+
+function applyDarkModeStyles() {
+    var isDarkModePreferred = localStorage.getItem("darkMode") === "true";
+
+    var body = document.body;
+    body.classList.toggle("dark-mode", isDarkModePreferred);
+
+    var darkElements = document.querySelectorAll(".dark-element");
+    darkElements.forEach(function(element) {
+        element.classList.toggle("dark-mode", isDarkModePreferred);
+    });
+
+    body.style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", applyDarkModeStyles);
--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,7 @@
-const mainnav = document.querySelector(".navbar");
-const hamburgerButton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation')
+const hamburgerButton = document.querySelector ('#menu');
 
 hamburgerButton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
-    hamburgerButton.classList.toggle('show');
-});
+    hamburgerButton.classList.toggle('show')
+})
