e014fcdd48ba077d04d30806add4042267c65925
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-11-02 16:11:33 +0200

    updated form.html

--- scripts/rating.js
+++ scripts/rating.js
@@ -4,3 +4,17 @@ const pageRatingInput = document.getElementById('pageRating');
         pageRatingInput.addEventListener('input', function () {
             pageRatingOutput.textContent = this.value;
         });
+
+        function validateForm() {
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirmPassword").value;
+            var passwordError = document.getElementById("passwordError");
+
+            if (password !== confirmPassword) {
+                passwordError.style.display = "block"; // Display the error message
+                return false; // Prevent form submission
+            } else {
+                passwordError.style.display = "none"; // Hide the error message
+                return true;
+            }
+        }
