7d4550984eb7a28ea98a8b6815b0187e287d3a13
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-11-08 18:16:32 +0200

    links.js changes

--- scripts/links.js
+++ scripts/links.js
@@ -1,31 +1,41 @@
-const baseURL = "https://hbaustin.github.io/wdd230/"; 
-const linksURL = "https://hbaustin.github.io/wdd230/data/links.json"; 
+// Define baseURL and linksURL
+const baseURL = 'https://hbaustin.github.io/wdd230/';
+const linksURL = baseURL + 'data/links.json';
 
-
+// Fetch and display links data
 async function getLinks() {
+  try {
     const response = await fetch(linksURL);
     const data = await response.json();
     displayLinks(data);
+  } catch (error) {
+    console.error('Error fetching links:', error);
   }
-  
+}
 
-getLinks();
-
+// Function to display links
 function displayLinks(weeks) {
-  const linksContainer = document.querySelector('.links-container'); 
+  const linksContainer = document.getElementById('links-container'); 
+
   weeks.forEach((week) => {
-    const weekElement = document.createElement('div');
-    weekElement.classList.add('week'); 
-    weekElement.innerHTML = `<h3>Week ${week.lesson}:</h3>`;
-    
+    const weekHeader = document.createElement('h2');
+    weekHeader.textContent = `Week ${week.week}`;
+
+    const linksList = document.createElement('ul');
+
     week.links.forEach((link) => {
-      const linkElement = document.createElement('a');
-      linkElement.href = `${baseURL}${link.url}`;
-      linkElement.target = '_blank';
-      linkElement.textContent = link.title;
-      weekElement.appendChild(linkElement);
+      const listItem = document.createElement('li');
+      const linkItem = document.createElement('a');
+      linkItem.textContent = link.title;
+      linkItem.href = link.url;
+      linkItem.target = '_blank'; 
+      listItem.appendChild(linkItem);
+      linksList.appendChild(listItem);
     });
 
-    linksContainer.appendChild(weekElement);
+    linksContainer.appendChild(weekHeader);
+    linksContainer.appendChild(linksList);
   });
 }
+
+getLinks();
