47c221cb6a3249735267877f336ee6f106fb20ef
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-11-08 18:37:56 +0200

    change home page to use json file

--- scripts/links.js
+++ scripts/links.js
@@ -1,41 +1,51 @@
-// Define baseURL and linksURL
 const baseURL = 'https://hbaustin.github.io/wdd230/';
 const linksURL = baseURL + 'data/links.json';
 
-// Fetch and display links data
 async function getLinks() {
   try {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.lessons);
   } catch (error) {
     console.error('Error fetching links:', error);
   }
 }
 
-// Function to display links
-function displayLinks(weeks) {
-  const linksContainer = document.getElementById('links-container'); 
-
-  weeks.forEach((week) => {
-    const weekHeader = document.createElement('h2');
-    weekHeader.textContent = `Week ${week.week}`;
-
-    const linksList = document.createElement('ul');
-
-    week.links.forEach((link) => {
-      const listItem = document.createElement('li');
-      const linkItem = document.createElement('a');
-      linkItem.textContent = link.title;
-      linkItem.href = link.url;
-      linkItem.target = '_blank'; 
-      listItem.appendChild(linkItem);
-      linksList.appendChild(listItem);
+function displayLinks(lessons) {
+    const linksContainer = document.getElementById('links-container');
+  
+    let lessonHeading = "";
+  
+    lessons.forEach((lesson) => {
+      const lessonDiv = document.createElement('div');
+      lessonDiv.style.marginBottom = '10px';
+  
+      if (lesson.lesson !== lessonHeading) {
+        lessonHeading = lesson.lesson;
+  
+        const lessonText = document.createElement('span');
+        lessonText.textContent = `Lesson ${lesson.lesson}: `;
+        lessonDiv.appendChild(lessonText);
+      }
+  
+      lesson.links.forEach((link, index) => {
+        const linkItem = document.createElement('a');
+        linkItem.textContent = link.title;
+        linkItem.href = link.url;
+        linkItem.target = '_blank';
+        linkItem.style.color = 'purple';
+  
+        lessonDiv.appendChild(linkItem);
+  
+        if (index !== lesson.links.length - 1) {
+          const separator = document.createElement('span');
+          separator.textContent = ' | ';
+          lessonDiv.appendChild(separator);
+        }
+      });
+  
+      linksContainer.appendChild(lessonDiv);
     });
-
-    linksContainer.appendChild(weekHeader);
-    linksContainer.appendChild(linksList);
-  });
 }
 
 getLinks();
