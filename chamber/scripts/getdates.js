747b7c8bba10b5a557e0e5212a3817dcfcd6d188
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-11-27 22:45:33 +0200

    corrected getdates.js file error

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,26 +1,27 @@
-const currentYear = new Date().getFullYear();
-document.querySelector.innerHTML = `&copy; ${currentYear}`;
-const lastModifiedDate = new Date(document.lastModified);
-document.querySelector('footer p:nth-child(2)').innerHTML = `Last Modified: ${lastModifiedDate.toLocaleString()}`;
-
 document.addEventListener("DOMContentLoaded", function () {
     const currentDate = new Date();
     const currentMillis = currentDate.getTime();
     const lastVisitMillis = parseInt(localStorage.getItem("lastVisit"));
 
-    if (!lastVisitMillis) {
-        document.getElementById("sidebar-message").textContent = "Welcome! Let us know if you have any questions.";
-    } else {
-        const oneDayMillis = 24 * 60 * 60 * 1000;
-        const daysSinceLastVisit = Math.floor((currentMillis - lastVisitMillis) / oneDayMillis);
-
-        if (daysSinceLastVisit < 1) {
-            document.getElementById("sidebar-message").textContent = "Back so soon! Awesome!";
+    const sidebarMessage = document.getElementById("sidebar-message");
+    
+    if (sidebarMessage) {
+        if (!lastVisitMillis) {
+            sidebarMessage.textContent = "Welcome! Let us know if you have any questions.";
         } else {
-            const message = `You last visited ${daysSinceLastVisit} ${daysSinceLastVisit === 1 ? 'day' : 'days'} ago.`;
-            document.getElementById("sidebar-message").textContent = message;
-        }
-    }
+            const oneDayMillis = 24 * 60 * 60 * 1000;
+            const daysSinceLastVisit = Math.floor((currentMillis - lastVisitMillis) / oneDayMillis);
 
-    localStorage.setItem("lastVisit", currentMillis.toString());
+            if (daysSinceLastVisit < 1) {
+                sidebarMessage.textContent = "Back so soon! Awesome!";
+            } else {
+                const message = `You last visited ${daysSinceLastVisit} ${daysSinceLastVisit === 1 ? 'day' : 'days'} ago.`;
+                sidebarMessage.textContent = message;
+            }
+        }
+
+        localStorage.setItem("lastVisit", currentMillis.toString());
+    } else {
+        console.error("Element with ID 'sidebar-message' not found.");
+    }
 });
