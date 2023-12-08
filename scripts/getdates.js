258ca686d3b85bbe0ed886eda47cfa9e459fb4de
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-12-08 15:47:28 +0200

    added base reserve form and placeholder redirect

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,9 @@ const currentYear = new Date().getFullYear();
 document.querySelector.innerHTML = `&copy; ${currentYear}`;
 const lastModifiedDate = new Date(document.lastModified);
 document.querySelector('footer p:nth-child(2)').innerHTML = `Last Modified: ${lastModifiedDate.toLocaleString()}`;
+
+const dateInput = document.getElementById('date');
+
+dateInput.addEventListener('click', () => {
+    dateInput.click(); 
+});
