6b882ea79a7aab17a094f0b7bd5739a85988a26f
HBAustin <64464394+HBAustin@users.noreply.github.com>
2023-12-08 12:44:28 +0200

    mostly completed rent.html page

---
+++ scoots/scripts/fullRental.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    try {
+        const response = await fetch('data/fullRental.json');
+        const data = await response.json();
+
+        const scooters = data.scooters;
+        const atvs = data.ATV;
+        const cars = data.cars;
+
+        const scooterSection = document.getElementById('scooterSection');
+        scooters.forEach(scooter => {
+            const { type, max, halfDay_reserve, fullDay_reserve, halfDay_walk, fullDay_walk, image } = scooter;
+            const scooterElement = document.createElement('div');
+            scooterElement.innerHTML = `
+                <h3>${type}</h3>
+                <p>Max: ${max}</p>
+                <p>Half Day (Reserve): $${halfDay_reserve}</p>
+                <p>Full Day (Reserve): $${fullDay_reserve}</p>
+                <p>Half Day (Walk-in): $${halfDay_walk}</p>
+                <p>Full Day (Walk-in): $${fullDay_walk}</p>
+                <img src="${image}" alt="${type}" />
+            `;
+            scooterSection.appendChild(scooterElement);
+        });
+
+        const atvSection = document.getElementById('atvSection');
+        atvs.forEach(atv => {
+            const { type, max, halfDay_reserve, fullDay_reserve, halfDay_walk, fullDay_walk, image } = atv;
+            const atvElement = document.createElement('div');
+            atvElement.innerHTML = `
+                <h3>${type}</h3>
+                <p>Max: ${max}</p>
+                <p>Half Day (Reserve): $${halfDay_reserve}</p>
+                <p>Full Day (Reserve): $${fullDay_reserve}</p>
+                <p>Half Day (Walk-in): $${halfDay_walk}</p>
+                <p>Full Day (Walk-in): $${fullDay_walk}</p>
+                <img src="${image}" alt="${type}" />
+            `;
+            atvSection.appendChild(atvElement);
+        });
+
+        const carSection = document.getElementById('carSection');
+        cars.forEach(car => {
+            const { type, max, halfDay_reserve, fullDay_reserve, halfDay_walk, fullDay_walk, image } = car;
+            const carElement = document.createElement('div');
+            carElement.innerHTML = `
+                <h3>${type}</h3>
+                <p>Max: ${max}</p>
+                <p>Half Day (Reserve): $${halfDay_reserve}</p>
+                <p>Full Day (Reserve): $${fullDay_reserve}</p>
+                <p>Half Day (Walk-in): $${halfDay_walk}</p>
+                <p>Full Day (Walk-in): $${fullDay_walk}</p>
+                <img src="${image}" alt="${type}" />
+            `;
+            carSection.appendChild(carElement);
+        });
+
+    } catch (error) {
+        console.error('Error fetching rental data:', error);
+    }
+});
